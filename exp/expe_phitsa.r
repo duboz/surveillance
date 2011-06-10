53f076b0990ff24f9d485483779866c35bc352d0
Bruno Bonté <bruno.bonte@cirad.fr>
2011-06-10 16:36:33 +0200

    set parameters to have an epidemics

--- exp/expe_phitsa.r
+++ exp/expe_phitsa.r
@@ -1,16 +1,18 @@
 source("phitsa-surveillance-functions.r")
 #Général
-duration=180
+duration=60
 
 #État initial
-infected_villages<-c("650205_10")
+infected_villages<-c("650408_01")
 
 #Paramètres
-distance_infectieuse <- 3500 #Distance d'infection (En mètres donc)
-rate = 0.3
-infper = 5
-recovper = 5
-p = 0.1 #proba de déclaration
+distance_infectieuse <- 4500 #Distance d'infection (En mètres donc)
+rate = 0.8
+infper = 7
+restockper = 7
+securedper = 7
+controlDelay = 2
+p = 0.01 #proba de déclaration
 
 #Données
 villages <- read.table("../data/villages-data.csv", sep=";", header=TRUE)
@@ -20,24 +22,12 @@ phitsa_netw<-create_phitsa_graph(distance_infectieuse)
 
 #formatage de l'état initial
 initState<-matrix(0,length(phitsa_netw[1,]),1)
+if (length(which(villages$VIL_CODE %in% infected_villages)) == 0) {
+	print("ERROR: initial outbreak not found..\n \n \n")
+}
 initState[which(villages$VIL_CODE %in% infected_villages),1]<-1
 
 #simulation
 res= controled_disease(phitsa_netw,initState, 
-rate, duration,infper, recovper, probaDeclaration=p)
-
-#Ploter le graphe (nécessite la librairie SNA)
-library(sna)
-coordinates <- villages$XX
-coordinates <- cbind(coordinates, villages$YY)
-grandes_villes <- c("") #Pour afficher les noms des grandes villes
-vill_label <- matrix("",1044,1)
-for (i in 1:1044)
-{if (villages$VIL_CODE[i] %in% grandes_villes) 
-	{vill_label[i] <- as.character(villages$VIL_ENG[i])
-	 print(villages$VIL_ENG[i])}
-	}
-
-gplot(phitsa_netw, gmode ="graph", coord=coordinates, 
-label = vill_label, boxed.labels=FALSE, label.bg=TRUE,
-label.cex=1, vertex.cex = 0.4, vertex.col="yellow")
+rate, duration,infper, securedper, restockper, probaDeclaration=p, controlDelay
+= controlDelay)
--- exp/phitsa-surveillance-functions.r
+++ exp/phitsa-surveillance-functions.r
@@ -2,19 +2,21 @@ library(rvle)
 
 #Fonction de simulation AVEC contrôle..
 controled_disease<-function(graph, infectedNodes, transmissionRate, 
-                      duration, infPeriode, recovPeriode, probaDeclaration =
-0.2, constPeriods = FALSE, control = TRUE){
+                      duration, infPeriode, securedPeriode, restockPeriode,
+controlDelay, probaDeclaration =
+0.2, constPeriods = TRUE, control = TRUE){
 dir<-getwd()
   f = rvle.open("phitsa-disease-surveillance-control-R.vpz", pkg="surveillance")
   setwd(dir)
   rvle.setDuration(f,duration)
 	rvle.setSeed(f,runif(1)*1000000)
  	rvle.setBooleanCondition(f,"control","disabled", !control)
- 	rvle.setRealCondition(f,"control","controlDelay", 1)
+ 	rvle.setRealCondition(f,"control","controlDelay", controlDelay)
  	rvle.setRealCondition(f,"passive_surv","probabilityDeclaration",
 probaDeclaration)
  	rvle.setRealCondition(f,"disease","infectiousPeriod", infPeriode)
-	rvle.setRealCondition(f,"disease","securedPeriod", recovPeriode)
+	rvle.setRealCondition(f,"disease","securedPeriod", securedPeriode)
+	rvle.setRealCondition(f,"disease","restockingPeriod", restockPeriode)
 	rvle.setBooleanCondition(f,"disease","constPeriods", constPeriods)
 	rvle.setRealCondition(f,"transmission","rate", transmissionRate)
 	classes<- ""
@@ -47,65 +49,9 @@ col.names = F)
 return (result)
 }
 
-##INITIALISATION
-
-#Génération du graphe:
-#graph=sociomatrix_as_random_graph(nb,proba=0.1) 
-#graph<-sociomatrix_as_regular_lattice(10,10) #(x*y = nb)
-#graph<-sociomatrix_as_rewired_lattice(x,y,proba) #(x*y = nb)
-#graph<-sociomatrix_as_scale_free(nb,1)
-source("generate-graphs.r")
-
-x=100
-g<-sociomatrix_as_rewired_lattice(10,10, p = 0.01) #(x*x = nb)
-longueDist<-1
-for (i in 1:longueDist){
-    g[runif(1)*x,runif(1)*x]<-1
-}
-initState<-matrix(0,x,1)
-initState[1,1]<-1
-rate=0.3
-duration=500
-infper=5
-recovper=5
-
-##SIMULATION
-test_uncontroled_disease<-function () 
-{
-#conditions initiales d'infection (exemple):
-
-res<-uncontroled_disease(g,initState, rate, duration,infper, recovper)
-infectious = c()
-for (t in 0:duration)
-  {
-	infectious =
-		c(infectious, length(which(res[[3]][t,2:length(initState)
-		+1]==2)))}
-  plot(res[[3]][,1], infectious)
-  return(res)
-}
-
-test_controled_disease<-function () 
-{
-res<-controled_disease(g,initState, rate, duration,infper, recovper)
-infectious = c()
-for (t in 0:duration)
-  {
-	infectious =
-		c(infectious, length(which(res[[3]][t,2:length(initState)
-		+1]==2)))}
-  plot(res[[3]][,1], infectious)
-  lines(res[[3]][,1], infectious)
-  return(res)
-}
-
-
-##Analyse
-
-
 ##Fonctions de visualisation
 library(sna)
-show_epidemic<-function(r,graph, coordo = FALSE){
+show_epidemic<-function(r,graph, coordo = FALSE, date = 1){
 if (coordo == FALSE) {
 par<-list(niter=1000)
 coordo=gplot.layout.fruchtermanreingold(graph,par)}
@@ -116,9 +62,9 @@ infection<-r[[3]][1,2:(nbNodes+1)]
 for (i in 1:nbNodes){
 colors[1,i]<-cols[(4-infection[i])]
 }
-gplot(graph,coord=coordo,vertex.col=r[[3]][1,2:(nbNodes+1)],arrowhead.cex = 0.1)
-gplot(graph,coord=coordo,vertex.col=colors,arrowhead.cex = 0.1)
-for (i in c(1:length(r[[3]][,1]))){
+#gplot(graph,coord=coordo,vertex.col=r[[3]][1,2:(nbNodes+1)],arrowhead.cex = 0.1)
+#gplot(graph,coord=coordo,vertex.col=colors,arrowhead.cex = 0.1)
+for (i in c(date:length(r[[3]][,1]))){
 infection<-r[[3]][i,2:(nbNodes+1)]
 colors<-matrix(0,1,nbNodes)
 for (j in 1:nbNodes){
@@ -131,6 +77,24 @@ main=paste("Epidemic at time",i))
 }
 
 #Fonction pour phitsanuloc
+
+#Ploter le graphe (nécessite la librairie SNA)
+plot_nice_netw<-function(phitsa_netw){
+library(sna)
+coordinates <- villages$XX
+coordinates <- cbind(coordinates, villages$YY)
+grandes_villes <- c("") #Pour afficher les noms des grandes villes
+vill_label <- matrix("",1044,1)
+for (i in 1:1044)
+{#if (villages$VIL_CODE[i] %in% grandes_villes) 
+	vill_label[i] <- as.character(villages$VIL_CODE[i])
+	 #print(villages$VIL_ENG[i])
+	}
+
+gplot(phitsa_netw, gmode ="graph", coord=coordinates, displayLabel =TRUE,
+label = vill_label, boxed.labels=FALSE, label.bg=TRUE,
+label.cex=1, vertex.cex = 0.4, vertex.col="yellow")
+}
 
 create_phitsa_graph <-function(inf_distance) {
 #Génération du graphe par proximité
